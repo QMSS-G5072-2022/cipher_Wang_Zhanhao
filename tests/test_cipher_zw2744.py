8d1774402d03bf00b73dbd698741fba896c471f6
tylerwang03 <tylerwang03@gmail.com>
2022-10-29 23:30:33 -0400

    pytest

--- tests/test_cipher_zw2744.py
+++ tests/test_cipher_zw2744.py
@@ -1,10 +1,9 @@
 from cipher_zw2744 import cipher_zw2744
-@pytest.mark.parametrize("actual, shift, expected", [
-    ('a', 2, 'c'),
-    ('abc',2,'cde'),
-    ('ABC',2,'CDE'),
-    ('a b c',2,'c d e')
-])
-def test_cipher_looping_(actual, shift, expected):
-    result = cipher_zw2744.cipher(actual, shift)
-    assert result == expected
+
+def test_cipher_una_symbols():
+    text = 'a//b'
+    shift = 2
+    expected = 'c//d'
+    actual = cipher_zw2744.cipher(text,shift)
+    assert expected==actual
+
